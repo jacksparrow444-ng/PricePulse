dba613b81532ca9fba0752a2bb2963a4c14aa4ba
Nirmal Gupta <ncbnirma@gmail.com>
2026-04-01 01:19:56 +0530

    debug: trying non-SSL database connection on Render

--- pricepulse-backend/config/db.js
+++ pricepulse-backend/config/db.js
@@ -6,12 +6,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false,
-    minVersion: 'TLSv1.2',
-    servername: process.env.DB_HOST,
-    ca: null
-  },
+  ssl: false,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
